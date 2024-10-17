80730905eb9e6bb607956b31eeda399dd4d4367e
Joao Rangel <joaohcrangel@gmail.com>
2024-10-17 13:26:58 -0300

    add isNullable

--- src/functions/foreignColumn.ts
+++ src/functions/foreignColumn.ts
@@ -1,10 +1,16 @@
 import { TableColumn } from 'typeorm';
 
-export const foreignColumn = (name: string, isPrimary = false) => {
+export const foreignColumn = ({
+  name,
+  isPrimary = false,
+  isNullable = false,
+}: TableColumn) => {
   return {
     name,
     type: 'int',
     unsigned: true,
     isPrimary,
+    isNullable,
+    default
   } as TableColumn;
 };
