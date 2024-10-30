f9a87e6cbb043dc8ea077b1a8b73f829986b4ac8
Joao Rangel <joaohcrangel@gmail.com>
2024-10-30 00:45:15 -0300

    fix formatDataWithLocale

--- src/functions/formatDataWithLocale.ts
+++ src/functions/formatDataWithLocale.ts
@@ -1,5 +1,7 @@
 export const formatDataWithLocale = (data: any) => {
-  const newData: any = {};
+  const newData: any = {
+    locales: {},
+  };
 
   for (const key of Object.keys(data)) {
     const keys = key.split('-');
@@ -7,8 +9,8 @@ export const formatDataWithLocale = (data: any) => {
     if (keys.length === 1) {
       newData[keys[0]] = data[keys[0]];
     } else {
-      if (!newData[keys[0]]) {
-        newData.locales = {};
+      if (!newData.locales[keys[0]]) {
+        newData.locales[keys[0]] = {};
       }
 
       newData.locales[keys[0]][keys[1]] = data[key];
