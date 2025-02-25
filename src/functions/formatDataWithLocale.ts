2c7615e01dcd756e4db50e23119a74a6b194312c
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-02-25 09:40:45 -0300

    feat: aprimorar formatação de dados com suporte a campos ausentes

--- src/functions/formatDataWithLocale.ts
+++ src/functions/formatDataWithLocale.ts
@@ -3,21 +3,38 @@ export const formatDataWithLocale = (data: any) => {
     locale: {},
   };
 
+  const locales = new Set<string>();
+  const fieldKeys = new Set<string>();
+
   for (const key of Object.keys(data)) {
-    if (key !== 'locale') {
-      const keys = key.split('-');
+    if (key !== "locale") {
+      const keys = key.split("-");
 
       if (keys.length === 1) {
         newData[keys[0]] = data[keys[0]];
       } else {
-        if (!newData.locale[keys[0]]) {
-          newData.locale[keys[0]] = {};
+        const locale = keys[0];
+        const field = keys[1];
+
+        if (!newData.locale[locale]) {
+          newData.locale[locale] = {};
         }
 
-        newData.locale[keys[0]][keys[1]] = data[key];
+        newData.locale[locale][field] =
+          data[key] !== undefined ? data[key] : "";
+        locales.add(locale);
+        fieldKeys.add(field);
       }
     }
   }
+
+  locales.forEach((locale) => {
+    fieldKeys.forEach((key) => {
+      if (!newData.locale[locale][key]) {
+        newData.locale[locale][key] = "";
+      }
+    });
+  });
 
   return newData;
 };
